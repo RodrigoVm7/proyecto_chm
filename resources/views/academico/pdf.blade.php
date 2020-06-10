fd3ca42a0eb704f7903d02a256550ac97cfa21cd
marceloR <marceloroa19@gmail.com>
2020-06-09 22:20:15 -0400

    Arreglos vista reportes por facultad v1

--- resources/views/academico/pdf.blade.php
+++ resources/views/academico/pdf.blade.php
@@ -5,17 +5,14 @@
 	<title></title>
 
 	<style>
-		html {
-			font-size: 9px;
-		}
-
 		table, td, th {
   			border: 1px solid black;
-			border-collapse:collapse;
 			padding: 0;
-			font-size: 12px;
+			border-collapse: collapse;
 		}
-		
+		/* th {
+  			height: 50px;
+		} */
     	#vertical-bar {
         	border-left: 1px solid #ccc;
         	width:1px;
@@ -35,11 +32,10 @@
             line-height: 30px;
         }
         div.noticia {
-  			background-color: #fff;
+  			background-color: white;
   			color: #6c6c6c;
-  			/* padding: 10px; */
+  			padding: 15px;
 		}
-
 		div.noticia img.izquierda {
   			float: left;
   			margin-right: 15px;
@@ -54,13 +50,11 @@
 </div>
 </header>
 <br><br>
-
-
-<body style="width: 100%;">
+<body>
 	<main><h3><center><u><b>PAUTA RESUMEN: Profesor {{$academico->categoria}}</b></u></center></h3></main>
-<h4><u>I. Identificación:</u></h4>
-<div class="w3-container" style="text-align:center; width: 100%;">
-	<table class="w3-table-all" style="width: 100%;">
+<h4><u>I. IDENTIFICACIÓN:</u></h4>
+<div class="w3-container">
+	<table class="w3-table-all" WIDTH="30%" style="font-size: 12px">
 		<tbody>
 			<tr>
 				<th style="text-align: center">Académico</th>
@@ -116,8 +110,8 @@
 <!-- <div class="page-break"></div> -->
 
 <h4>II. Calificación Académica:</h4>
-<div>
-	<table style="margin: 0 auto;">
+<div class="w3-container">
+	<table>
 			<tr>
 				<th rowspan="2"></th>
 				<th rowspan="2">% TIEMPO ASIGNADO A TAREAS PROGRAMADAS</th>
@@ -133,7 +127,7 @@
 				<th>% T x C / 100</th>
 			</tr>
 			<tr>
-				<td>1. Actividades de Docencia</td>
+				<td style="width: 35%; text-align: center;">1. Actividades de Docencia</td>
 				<td><center>{{$datos->tiempoActividad1}}</center></td>
 				<td><center>{{$datos->e1}}</center></td>
 				<td><center>{{$datos->mb1}}</center></td>
@@ -143,7 +137,7 @@
 				<td><center>{{$datos->t1c}}</center></td>
 			</tr>
 			<tr>
-				<td>2. Actividades de Investigación</td>
+				<td style="text-align: center;">2. Actividades de Investigación</td>
 				<td><center>{{$datos->tiempoActividad2}}</center></td>
 				<td><center>{{$datos->e2}}</center></td>
 				<td><center>{{$datos->mb2}}</center></td>
@@ -153,7 +147,7 @@
 				<td><center>{{$datos->t2c}}</center></td>
 			</tr>
 			<tr>
-				<td>3. Extensión y Vinculación</td>
+				<td style="text-align: center;">3. Extensión y Vinculación</td>
 				<td><center>{{$datos->tiempoActividad3}}</center></td>
 				<td><center>{{$datos->e3}}</center></td>
 				<td><center>{{$datos->mb3}}</center></td>
@@ -163,7 +157,7 @@
 				<td><center>{{$datos->t3c}}</center></td>
 			</tr>
 			<tr>
-				<td>4. Administración Académica</td>
+				<td style="text-align: center;">4. Administración Académica</td>
 				<td><center>{{$datos->tiempoActividad4}}</center></td>
 				<td><center>{{$datos->e4}}</center></td>
 				<td><center>{{$datos->mb4}}</center></td>
@@ -173,7 +167,7 @@
 				<td><center>{{$datos->t4c}}</center></td>
 			</tr>
 			<tr>
-				<td>5. Otras Actividades Realizadas</td>
+				<td style="text-align: center;">5. Otras Actividades Realizadas</td>
 				<td><center>{{$datos->tiempoActividad5}}</center></td>
 				<td><center>{{$datos->e5}}</center></td>
 				<td><center>{{$datos->mb5}}</center></td>
@@ -189,19 +183,18 @@
 	</table>
 </div>
 
-<br><br>
 <h4>III. ESCALA EVALUATIVA:</h4>
 <div class="w3-container">
 	<table class="w3-table-all" WIDTH="30%">
 			<tr>
-				<th style="font-size: 14px;"><center>ESCALA:Excelente = 4,5 a 5      Muy Bueno = 4,0 a 4,4<br>Regular = 3,4 a 2,7    Deficiente = menos de 2,7 Bueno: 3,5 a 3,9</center></th>
+				<th><center>ESCALA:Excelente = 4,5 a 5      Muy Bueno = 4,0 a 4,4<br>Regular = 3,4 a 2,7    Deficiente = menos de 2,7 Bueno: 3,5 a 3,9</center></th>
 			</tr>
 	</table>
 </div>
 
-<br>
 <div class="page-break"></div>
-<h4 style="margin-top: 30px;">IV. ARGUMENTOS DE LA CALIFICACIÓN FINAL:</h4>
+<br><br><br>
+<h4>IV. ARGUMENTOS DE LA CALIFICACIÓN FINAL:</h4>
 <div class="w3-container">
 	<table class="w3-table-all" WIDTH="30%">
 			<tr>
@@ -209,6 +202,7 @@
 			</tr>
 	</table>
 </div>
+
 <br>
 <h4><center>FIRMA COMISIÓN</center></h4>
 <div class="w3-container">
@@ -225,10 +219,10 @@
 			<th><center>Nombre y Firma</center></th>
 		</tr>
 	</table>
-<br>
-	<table class="w3-table-all" WIDTH="30%">
+
+	<table class="w3-table-all" WIDTH="30%" style="margin-top: 10px;">
 		<tr hidden=""></tr>
-		<tr>
+		<tr style="margin-top: 3px;">
 			<td style="visibility:hidden;"><br><br></td>
 			<td style="visibility:hidden;"><br><br></td>
 			<td><br><br></td>
@@ -236,11 +230,10 @@
 		<tr>
 			<th style="visibility:hidden;"><center>Nombre y Firmaaaaaaa</center></th>
 			<th style="visibility:hidden;"><center>Nombre y Firmaaaaaa</center></th>
-			<th><center>Nobre y Firma Ministro de Fe</center></th>
+			<th style="width: 50%;"><center>Nombre y Firma Ministro de Fe</center></th>
 		</tr>
 	</table>
+	<br><h8>FECHA:</h8>
 </div>
-<br><h8>FECHA:</h8>
-
 </body>
 </html>
